411ce2a74b5bfba1ad5db4e77a558a64eed8610e
hemavathiravi <52271619+hemavathiravi@users.noreply.github.com>
2019-06-28 15:00:35 +0530

    Create const.py

---
+++ const.py
@@ -0,0 +1,7 @@
+ch = input("enter a character: ")
+
+if(ch=='A' or ch=='a' or ch=='E' or ch=='e' or ch=='i' or ch=='I' or ch=='o'
+   or ch=='u' or ch=='U'):
+		print(ch,"is a vowel")
+else:
+		print(ch,"is a constant")
